4c5fe404304fb25b6dbc99027b48c7fc5b26e787
Eduardo Batista <dudubatista@gmail.com>
2020-10-31 03:23:15 -0300

    Simulation section hidden before bugs are solved.

--- backend/simulate.sh
+++ backend/simulate.sh
@@ -20,9 +20,9 @@ if [ -x "$(command -v ghdl)" ]; then
     ghdl -e -fexplicit --ieee=synopsys usertest
     ghdl -r usertest --wave=output.ghw --stop-time=$2
 else 
-    /opt/local/bin/ghdl -a -fexplicit --ieee=synopsys ${@:2}
+    /opt/local/bin/ghdl -a -fexplicit --ieee=synopsys ${@:3}
     /opt/local/bin/ghdl -e -fexplicit --ieee=synopsys usertest
-    /opt/local/bin/ghdl -r usertest --vcd=output.ghw
+    /opt/local/bin/ghdl -r usertest --wave=output.ghw --stop-time=$2
 fi
 
 
