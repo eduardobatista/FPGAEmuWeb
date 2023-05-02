e36f619aa06b54dc058c662b6588566d28d063fa
Eduardo Batista <dudubatista@gmail.com>
2023-05-01 22:38:31 -0300

    Changing python package versions aiming to remove bug.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cron start
-
 rsync -a /home/fpgaemuweb/persistentwork/ /home/fpgaemuweb/work
+
+crontab /home/fpgaemuweb/crontask
+cron start
 
 /usr/bin/supervisord -c /home/fpgaemuweb/supervisord.conf
